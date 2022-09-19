562ac549b85ec7eda827686aed6cb96676393ce0
yagi1231 <at.sao.1231@gmail.com>
2022-09-19 18:47:04 +0900

    ログイン機能まで

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+
+    'failed' => 'メールアドレス、もしくはパスワードが違います',
+    'throttle' => 'ログイン試行回数がしきい値を超えました。 時間をおいてログインしてください。',
+    'login' => 'ログインしました',
+    'logout' => 'ログアウトしました',
+    'notVerified' => 'メールが認証されていません',
+    'verified' => 'メールの認証に成功しました',
+    'alreadyVerified' => '既に認証済みのメールアドレスです',
+    'sendVerifyMail' => '認証メールを再送しました',
+    'noUser' => '入力されたメールアドレスのユーザーが存在しません',
+
+    'email'            => 'メールアドレス',
+    'password'         => 'パスワード',
+    'confirm_password' => 'パスワードを確認',
+    'sign_in'          => 'ログイン',
+    'admin_sign_in'    => '管理者ログイン',
+    'sign_out'         => 'ログアウト',
+    'register'         => 'ユーザー登録',
+    'full_name'        => '氏名',
+
+    'login' => [
+        'forgot_password'     => 'パスワードを忘れた時',
+    ],
+
+    'forgot_password' => [
+        'send_pwd_reset' => 'パスワード変更リンクを送る',
+    ],
+
+    'reset_password' => [
+        'title'         => 'パスワードを変更',
+        'enter_email' => 'メールアドレスを入力してください'
+    ],
+
+    'registration' => [
+        'have_membership' => '既にアカウントを持っている方はこちら'
+    ]
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+];
